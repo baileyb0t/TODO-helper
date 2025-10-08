2f07dd90abcf9781d858bc7d785b91575a384395
Bailey <bailey@hrdag.org>
2025-10-08 06:37:29 -0700

    adding routine for summarizing a quarters worth of activity

---
+++ compose-board-updates/src/add-scheduled-meetings.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python3
+# vim: set ts=4 sts=0 sw=4 si fenc=utf-8 et:
+# vim: set fdm=marker fmr={{{,}}} fdl=0 foldcolumn=4:
+# Authors:     BP
+# Maintainers: BP
+# Copyright:   2023, HRDAG, GPL v2 or later
+# =========================================
+
+# dependencies --- {{{
+from pathlib import Path
+from sys import stdout
+import argparse
+import logging
+import yaml
+from zoneinfo import ZoneInfo
+from datetime import date, datetime
+from dateutil.relativedelta import *
+import pandas as pd
+from icalendar import Calendar
+import recurring_ical_events
+import doc
+# }}}
+
+IGNOREABLE = (
+    'birthday', 'celebration of life',
+    'appt', 'due',
+    'office hour', ' oh',
+    'time off', 'holiday', 'day off', 'moving', 'travel',
+    'window',
+    'inspection', 'diagnostic', 'car',
+    'hana', 'ty', 'miranda',
+)
+
+# support methods {{{
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rules", default="hand/rules.yml")
+    parser.add_argument("--json", default=None)
+    parser.add_argument("--ics", default="../calendar/output/mbp.ics")
+    parser.add_argument("--output", default=None)
+    args = parser.parse_args()
+    assert Path(args.rules).exists()
+    assert Path(args.json).exists()
+    assert Path(args.ics).exists()
+    return args
+
+
+def get_logger(sname, file_name=None):
+    logger = logging.getLogger(sname)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s " +
+                                  "- %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
+    stream_handler = logging.StreamHandler(stdout)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    if file_name:
+        file_handler = logging.FileHandler(file_name)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+    return logger
+
+
+def read_yaml(fname):
+    with open(fname, 'r') as f:
+        rules = yaml.safe_load(f)
+        f.close()
+    return rules
+
+
+def load_cal(icsname):
+    with open(icsname, 'rb') as f:
+        ecal = Calendar.from_ical(f.read())
+    f.close()
+    return ecal
+
+
+def format_dtstart(event, local='America/Los_Angeles'):
+    """When we get the event start time and compare to our 9-5 work hours,
+    some events get lost because they're scheduled in another timezone.
+
+    Note:
+    - event.decoded('DTSTART') returns a datetime.datetime object
+    - event.get('DTSTART') returns a vDDDTypes object
+
+    This thread might be a better longterm strategy:
+    - https://stackoverflow.com/questions/1111317/how-do-i-print-a-datetime-in-the-local-timezone
+
+    @TODO: Can you have timezone info for an all day event? Do time,tz always appear together?"""
+    dtdt_start = event.decoded('DTSTART')
+    if not isinstance(dtdt_start, datetime): return dtdt_start
+    if pd.isna(dtdt_start.tzinfo): return dtdt_start
+    formatted = dtdt_start.astimezone(ZoneInfo(local))
+    return formatted
+
+
+def get_event_info(event, dtstart):
+    """Structure and string-format the basic extracted event information so it's ready for the markdown note."""
+    if not isinstance(dtstart, datetime): time = '*all day*'
+    else: time = dtstart.time().strftime('%I:%M%p').lower()
+    return {
+        'title': str(event['SUMMARY']),
+        'date': dtstart.strftime('%a %b %d'),
+        'time': time,
+    }
+
+
+def find_events(ecal, caldate):
+    """Look on `ecal` for `caldate` events that are candidate scheduled meetings.
+
+    Handles recurring events that may not otherwise appear using `icalendar` save the first occurrence.
+    Note that events are only really considered if they have a title or 'SUMMARY' value."""
+    events = []
+    for event in recurring_ical_events.of(ecal).at(caldate):
+        dtstart = format_dtstart(event=event)
+        if isinstance(dtstart, datetime):
+            if (dtstart.hour < 9) | (dtstart.hour > 17): continue
+        if dtstart.isoweekday() > 5: continue
+        if 'SUMMARY' in event:
+            if any([kw.lower() in str(event['SUMMARY']).lower()
+                    for kw in IGNOREABLE]): continue
+            info = get_event_info(event, dtstart=dtstart)
+            if 'LOCATION' in event:
+                info['location'] = str(event['LOCATION'])
+                if any([kw in info['location'].lower() for kw in (
+                    'zoom', 'teams', 'http')]):
+                    meet_type = 'virtual'
+                else: meet_type = 'in person'
+                info['meet_type'] = meet_type
+            else:
+                info['location'] = 'no location set'
+                info['meet_type'] = 'no location set'
+            if event['SUMMARY'].lower() == 'payroll holiday':
+                info['location'], info['meet_type'] = '', ''
+            events.append(info)
+    return events
+
+
+def get_events(ecal, caldate):
+    """Gather the formatted core details of all candidate events and prepare collection for markdown note.
+
+    @TODO: Think on the division of work between the `get_events()`, `find_events()`, `get_event_info()`, etc."""
+    events = find_events(ecal, caldate)
+    out = []
+    for event in events:
+        text = f"{event['date']} at {event['time']}: "
+        if not any(event['meet_type']): text += event['title']
+        else: text += f"{event['title']} ({event['meet_type']})"
+        out.append(text)
+    return sorted(out)
+
+
+def add_events(notes, events, prefix):
+    """Insert gather events into note."""
+    if not any(events): return notes
+    for text in events:
+        notes.insert(prefix=prefix,
+                     text=text)
+    return notes
+# }}}
+
+# main --- {{{
+if __name__ == '__main__':
+    logger = get_logger(__name__, "output/add-scheduled-meetings.log")
+    args = get_args()
+
+    ecal = load_cal(args.ics)
+    rules = read_yaml(args.rules)
+    formats = rules['format']
+    notes = doc.from_json(args.json)
+
+    e = date.today().replace(day=1) - relativedelta(days=1)
+    s = (e - relativedelta(months=2)).replace(day=1)
+    ndays = len(pd.date_range(s, e))
+    logger.info(f'setting up meetings between {s} and {e}')
+
+    notes.insert(prefix=formats['header'], text='On the calendar')
+    notes.insert(prefix=formats['subheader'], text='Last quarter')
+    for i in range(0, ndays):
+        caldate = (s + relativedelta(days=+i)).strftime("%Y%m%d")
+        events = get_events(ecal, caldate)
+        notes = add_events(notes, events, prefix=formats['meeting_done'])
+    notes.insert(prefix=formats['text'], text='')
+
+    notes.to_json(args.output)
+# }}}
---
+++ compose-board-updates/src/start-simple.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# vim: set ts=4 sts=0 sw=4 si fenc=utf-8 et:
+# vim: set fdm=marker fmr={{{,}}} fdl=0 foldcolumn=4:
+# Authors:     BP
+# Maintainers: BP
+# Copyright:   2025, HRDAG, GPL v2 or later
+# =========================================
+
+# dependencies --- {{{
+from pathlib import Path
+from sys import stdout
+import argparse
+import logging
+import yaml
+from zoneinfo import ZoneInfo
+from datetime import date, datetime
+from dateutil.relativedelta import *
+from doc import Doc
+# }}}
+
+# support methods {{{
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rules", default="hand/rules.yml")
+    parser.add_argument("--date", default=None)
+    parser.add_argument("--outputdir", default="/Users/home/git/my-TODO/individual/board")
+    parser.add_argument("--output", default=None)
+    args = parser.parse_args()
+    assert Path(args.rules).exists()
+    return args
+
+
+def get_logger(sname, file_name=None):
+    logger = logging.getLogger(sname)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s " +
+                                  "- %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
+    stream_handler = logging.StreamHandler(stdout)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    if file_name:
+        file_handler = logging.FileHandler(file_name)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+    return logger
+
+
+def read_yaml(fname):
+    with open(fname, 'r') as f:
+        rules = yaml.safe_load(f)
+        f.close()
+    return rules
+
+
+def format_date(from_arg, date=datetime.now()):
+    if not from_arg:
+        return date.replace(tzinfo=ZoneInfo('US/Pacific'))
+    if '-' in date: form = datetime.strptime(date, '%Y-%m-%d')
+    else: form = datetime.strptime(date, '%Y%m%d')
+    return form.astimezone(ZoneInfo('US/Pacific'))
+
+
+def prep_out(givendate, outdir):
+    if not givendate: today = format_date(from_arg=False)
+    else: today = format_date(from_arg=True, date=givendate)
+    path = f"{outdir}/{today.strftime('%Y-%m-%d')}"
+    today = today
+    return path, today
+# }}}
+
+# main --- {{{
+if __name__ == '__main__':
+    logger = get_logger(__name__, "output/start-simple.log")
+    args = get_args()
+
+    rules = read_yaml(args.rules)
+    formats = rules['format']
+
+    # Define 'quarter' flexibly as prior three months
+    e = date.today().replace(day=1) - relativedelta(days=1)
+    s = (e - relativedelta(months=2)).replace(day=1)
+    logger.info(f'setting up activity between {s} and {e}')
+    path, today = prep_out(givendate=date.today().strftime("%Y%m%d"), outdir="./")
+    notes = Doc(prefix='# ',
+              text=f"BP updates from {s.strftime('%d %b')} to {e.strftime('%d %b')} {e.strftime('%Y')}",
+              path=path,
+              weeklyday=today.strftime('%Y-%m-%d'))
+    notes.insert(prefix=formats['text'], text='\n')
+
+    notes.insert(prefix=formats['header'], text='On my plate')
+    notes.insert(prefix=formats['subheader'], text='Priorities this quarter')
+    notes.insert(prefix=formats['notes'], text='\n')
+    notes.insert(prefix=formats['subheader'], text='Back-burner this quarter')
+    notes.insert(prefix=formats['notes'], text='\n')
+    notes.insert(prefix=formats['subheader'], text='Back-back-burner')
+    notes.insert(prefix=formats['notes'], text='\n\n')
+
+    notes.to_json(args.output)
+# }}}
